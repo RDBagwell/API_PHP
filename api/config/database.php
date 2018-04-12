574ea09db7da4e6022cdc8aaf117bfe825de16c2
Robert Bagwell <erbagwell@yahoo.com>
2018-04-12 17:41:19 -0600

    Add files via upload

--- api/config/database.php
+++ api/config/database.php
@@ -2,18 +2,39 @@
     require_once("new_config.php");
     
     class Database {
-        public $conn;
+
+        public $connection;
         public $db;
-               
-        public function DBConnection(){
-            $this->conn = NULL;
+
+        function __construct(){
+            $this->db = $this->db_open_connection();
+        }
+
+        public function db_open_connection(){
             try {
-                $this->conn = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME.";",DB_USER, DB_PASSWORD);
-                $this->conn->exec("set names utf8");
+                $this->connection = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME.";",DB_USER, DB_PASSWORD);
             } catch (PDOException $ex) {
                 echo "Connection error: " . $ex->getMessage();
             }
-            return $this->conn;
+            return $this->connection;
+        }
+
+        public function query($sql) {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function escape_string($string){
+            $sanitized_strin = htmlspecialchars(strip_tags($string));
+            return $sanitized_strin;
         }
         
-    }   
+        public function insert_id(){
+            return $this->db->lastInsertId();
+        }
+
+        
+    }  // End Database Class
+
+    $database = new Database();
